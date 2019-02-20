bf462180350781767729b5e52d8a1ab8b72bc5be
jason-derry <jason.derry@academytrainee.com>
2019-02-20 16:09:33 +0000

    initial commit

---
+++ src/main/java/com/qa/account/Account.java
@@ -0,0 +1,48 @@
+package com.qa.account;
+
+import javax.persistence.Id;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "sentAccount")
+public class Account {
+	
+	@Id
+	private Long accountId;
+	private String email;
+	private String fullName;
+	private Boolean playing;
+
+	public Long getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(Long accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isPlaying() {
+		return playing;
+	}
+
+	public void setPlaying(Boolean playing) {
+		this.playing = playing;
+	}
+	
+}
---
+++ src/main/java/com/qa/account/AccountRepository.java
@@ -0,0 +1,14 @@
+package com.qa.account;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+//@RepositoryRestResource(collectionResourceRel = "sentAccount", path = "sentAccount")
+public interface AccountRepository extends MongoRepository<Account, String> {
+
+	List<Account> findByFullName(@Param("fullName") String fullName);
+
+}
