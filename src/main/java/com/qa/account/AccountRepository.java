6c023aff1c3e18d3a3debc8ef8efe2b78b50de63
jason-derry <jason.derry@academytrainee.com>
2019-02-20 16:40:50 +0000

    connection with mongodb

--- src/main/java/com/qa/account/AccountRepository.java
+++ src/main/java/com/qa/account/AccountRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-//@RepositoryRestResource(collectionResourceRel = "sentAccount", path = "sentAccount")
+@RepositoryRestResource(collectionResourceRel = "sentAccount", path = "sentAccount")
 public interface AccountRepository extends MongoRepository<Account, String> {
 
 	List<Account> findByFullName(@Param("fullName") String fullName);
