f71416bd7325911191865d95674684f4da26386c
jason-derry <jason.derry@academytrainee.com>
2019-02-21 15:50:09 +0000

    removed querydsl

--- src/main/java/com/qa/account/persistence/repository/AccountRepository.java
+++ src/main/java/com/qa/account/persistence/repository/AccountRepository.java
@@ -4,12 +4,11 @@ import com.qa.account.persistence.domain.SentAccount;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository extends MongoRepository<SentAccount, Long>, QuerydslPredicateExecutor<SentAccount> {
+public interface AccountRepository extends MongoRepository<SentAccount, Long> {
 
 	@Query("{ 'accountId' : ?0 }")
 	SentAccount findAccountById(Long accountId);
